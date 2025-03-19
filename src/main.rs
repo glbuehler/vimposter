8967f0a4563a80f9527cb37d20452f17bb1c5f38
Gideon Bühler <gideonbuehler18@gmail.com>
2025-03-19 20:59:29 +0100

    added panic recovery hook

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use editor::*;
 
 fn main() {
+    let hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |info| {
+        exit();
+        println!("");
+        hook(info);
+    }));
+
     enter();
     run();
     exit();
